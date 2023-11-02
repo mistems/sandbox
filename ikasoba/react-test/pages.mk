b1abb2f5f09366b9f6d02a9e860c4f7ff01be101
ikasoba <57828948+ikasoba@users.noreply.github.com>
2023-11-02 23:07:03 +0900

    修正

--- ikasoba/react-test/pages.mk
+++ ikasoba/react-test/pages.mk
@@ -3,5 +3,5 @@ init:
 build:
 	mkdir -p .www/ikasoba/ddsk
 	deno run -A build.js
-	cp $(wildcard dist/*) $(repository_root)/.www/ikasoba/ddsk
+	cp $(wildcard .dist/*) $(repository_root)/.www/ikasoba/ddsk
 	cp src/index.html $(repository_root)/.www/ikasoba/ddsk
